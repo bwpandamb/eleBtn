91050246824a925b00bf7cbf010023f0e55af9dc
马斌 <bwpanda_d@163.com>
2018-12-22 13:08:12 +0800

    init

---
+++ app/src/main/java/com/charlesma/elebtn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.charlesma.elebtn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.florent37.viewanimator.ViewAnimator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
